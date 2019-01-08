f59f3ea9e36c081aea814008903f743830476ffe
alsoscope <sagittariuus@naver.com>
2019-01-08 16:23:31 +0900

    Spring4 RestController

---
+++ Spring4_00_RestController/src/main/java/org/p/project/controller/SampleController.java
@@ -0,0 +1,89 @@
+package org.p.project.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.p.project.domain.SampleVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
+
+//RestControlelr spring 4부터 지원 3부터는 ResponseBody
+//@RestController
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@RequestMapping("/hello")
+	//@ResponseBody
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/sendVO")
+	//@ResponseBody
+	public SampleVO sendVO() {
+		SampleVO vo=new SampleVO();
+		vo.setFirstName("길길");
+		vo.setLastName("동동");
+		vo.setMno(123);
+		return vo;
+	}
+	
+	//@ResponseBody
+	@RequestMapping("/sendList")
+	public List<SampleVO> sendList(){
+		List<SampleVO> list=new ArrayList<SampleVO>();
+		for(int i=0; i<10; i++) {
+			SampleVO vo=new SampleVO();
+			vo.setFirstName("무무");
+			vo.setLastName("민민");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return list;
+	}
+	
+	//@ResponseBody
+	@RequestMapping("/sendMap")
+	public Map<Integer, SampleVO> sendMap(){
+		Map<Integer, SampleVO> map=new HashMap<Integer, SampleVO>();
+		for(int i=0; i<=10; i++) {
+			SampleVO vo=new SampleVO();
+			vo.setFirstName("헬로");
+			vo.setLastName("키티");
+			vo.setMno(i);
+			map.put(i, vo);
+		}
+		return map;
+	}
+	
+	//ResponseEntity HTTP상태코드 제어. 예외적인 상황에서 문제 발생시 정보 전달
+	//@ResponseBody
+	@RequestMapping("/sendErrorAuth")
+	public ResponseEntity<Void> sendListAuth(){
+		//HTTP 상태(Status) 코드 전송
+		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+	}
+	
+	//HTTP상태 코드와 데이터를 함께 전달
+	//@ResponseBody
+	@RequestMapping("/sendErrorNot")
+	public ResponseEntity<List<SampleVO>> sendListNot(){
+		List<SampleVO> list=new ArrayList<SampleVO>();
+		for(int i=0; i<10; i++) {
+			SampleVO vo=new SampleVO();
+			vo.setFirstName("로로");
+			vo.setLastName("마마");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return new ResponseEntity<List<SampleVO>>(list, HttpStatus.NOT_FOUND);
+	}	
+}//class SampleController
